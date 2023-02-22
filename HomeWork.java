75c66310cf28b6a9160815cba84354dbec2a2cef
anushervonodilov <anushboy2000@gmail.com>
2023-02-22 16:23:10 -0500

    Selenium HW1

---
+++ HomeWork.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        driver.close();
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        String path = "https://www.google.com/";
+        driver.get(path);
+
+        String URL = driver.getCurrentUrl();
+        String title = driver.getTitle();
+
+        if (path.equalsIgnoreCase(URL)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No Match");
+        }
+
+        if (title.equalsIgnoreCase("Google")) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No Match");
+        }
+
+        driver.quit();
+
+    }
+}
